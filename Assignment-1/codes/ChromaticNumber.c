c281d6652537ec443d86da819bc5b2ccc9997c51
adyasa611 <47292629+adyasa611@users.noreply.github.com>
2021-08-30 17:34:52 +0530

    Update ChromaticNumber.c

--- Assignment-1/codes/ChromaticNumber.c
+++ Assignment-1/codes/ChromaticNumber.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
  
-// Define the number of vertices in the graph
+// Number of vertices in the graph
 #define N 6
 
+// Structure to define Graph
 struct Graph
 {
     struct Node* head[N];
 };
- 
+
+// Structure to define vertices
 struct Node
 {
     int destination;
     struct Node* next;
 };
- 
+
+// Structure to define the edge 
 struct Edge 
 {
     int source, destination;
 };
  
-
+// Function to create the graph
 struct Graph* createGraph(struct Edge edges[], int n)
 {
 
@@ -47,7 +50,7 @@ struct Graph* createGraph(struct Edge edges[], int n)
     return graph;
 }
 
-
+// Function to calculate Chromatic Number
 void calculateChromaticNumber(struct Graph* graph)
 {
     int allot[N];
@@ -113,19 +116,20 @@ void calculateChromaticNumber(struct Graph* graph)
             count++;
         }
     }
-    printf("Chromatic Number: %d \n",count);
+    printf("Chromatic Number: %d \n",count); // Print the Chromatic Number
 
 }
 
-
+// Main Function 
 int main(void)
 {
+    // Defining the edges in the graph
     struct Edge edges[] =
     {
         { 0, 1 }, {0,2}, {0,4}, {1,0}, {1,2}, {1,3}, {1,5}, {2,0}, {2,1}, {2,3}, {2,4}, {3,2}, {3,1}, {3,5}, {4,0}, {4,2}, {4,5}, {5,1}, {5,3}, {5,4}
     };
-    int n = sizeof(edges)/sizeof(edges[0]);
-
+    // Number of edges
+    int n = sizeof(edges)/sizeof(edges[0]); 
     struct Graph *graph = createGraph(edges, n);
     calculateChromaticNumber(graph);
     return 0;
